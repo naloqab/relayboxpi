f2ea32d6054b7669ba67f587d98dd7ca278740ab
Naser Aloqab <n.aloqab@gmail.com>
2018-10-10 18:24:33 -0500

    Changed GamingPCPower.py method from SSH to sockets

--- GamingPCPower.py
+++ GamingPCPower.py
@@ -1,31 +1,45 @@
 import sys
-import paramiko
+# import paramiko
+import socket
 
-ip = '192.168.1.100'
-port = 22
+host = '192.168.1.100'
+socketPort = 5009
+SSHPort = 22
 username = 'pi'
 password = 'Kaka2233!'
 
-bootGamingPC = 'python3 code/bootGamingPC.py'
-shutdownGamingPC = 'python3 code/shutdownGamingPC.py'
-# bootGamingPC = 'echo bootTest'
-# shutdownGamingPC = 'echo shutdownTest'
+bootGamingPC = b"python3 /home/pi/code/bootGamingPC.py"
+shutdownGamingPC = b"python3 /home/pi/code/shutdownGamingPC.py"
 
 if sys.argv[1] == 'ON':
     try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, port, username, password)
-        stdin, stdout, stderr = ssh.exec_command(bootGamingPC)
+        # ssh = paramiko.SSHClient()
+        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # ssh.connect(host, SSHPort, username, password)
+        # stdin, stdout, stderr = ssh.exec_command(bootGamingPC)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, socketPort))
+        s.sendall(bootGamingPC)
+        data = s.recv(1024)
+        s.close()
+
         PowerStatus = 'ON'
     except:
         PowerStatus = 'OFF'
 else:
     try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, port, username, password)
-        stdin, stdout, stderr = ssh.exec_command(shutdownGamingPC)
+        # ssh = paramiko.SSHClient()
+        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # ssh.connect(host, SSHPort, username, password)
+        # stdin, stdout, stderr = ssh.exec_command(shutdownGamingPC)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, socketPort))
+        s.sendall(shutdownGamingPC)
+        data = s.recv(1024)
+        s.close()
+
         PowerStatus = 'OFF'
     except:
         PowerStatus = 'ON'
