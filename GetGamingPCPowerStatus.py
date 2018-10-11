b669ec2308694081c7f5e62f0bf97f181defb9d4
Naser <n.aloqab@gmail.com>
2018-10-11 00:51:30 -0500

    Added GetGamingPCPowerStatus.py

---
+++ GetGamingPCPowerStatus.py
@@ -0,0 +1,23 @@
+import socket
+
+host = '192.168.1.100'
+socketPort = 5009
+
+command = "GamingPCPowerStatus"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, socketPort))
+s.sendall(command)
+data = s.recv(1024)
+s.close()
+
+PowerStatus = data
+
+logPath = 'GamingPCPowerStatus.txt'
+
+if not os.path.exists(logPath):
+    os.system("touch {}".format(logPath))
+
+StatusFile = open(logPath, 'w')
+StatusFile.write(PowerStatus)
+StatusFile.close()
