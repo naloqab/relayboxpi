24d66a13cf92aaa4036faa2c99b30bb05eb9fd41
Naser <n.aloqab@gmail.com>
2018-10-11 00:32:15 -0500

    Removed SSH code and left socket

--- GamingPCPower.py
+++ GamingPCPower.py
@@ -1,49 +1,19 @@
 import sys
-# import paramiko
 import socket
 
 host = '192.168.1.100'
 socketPort = 5009
-SSHPort = 22
-username = 'pi'
-password = 'Kaka2233!'
 
 bootGamingPC = b"python3 /home/pi/code/bootGamingPC.py"
 shutdownGamingPC = b"python3 /home/pi/code/shutdownGamingPC.py"
 
 if sys.argv[1] == 'ON':
-    try:
-        # ssh = paramiko.SSHClient()
-        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # ssh.connect(host, SSHPort, username, password)
-        # stdin, stdout, stderr = ssh.exec_command(bootGamingPC)
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, socketPort))
-        s.sendall(bootGamingPC)
-        data = s.recv(1024)
-        s.close()
-
-        PowerStatus = 'ON'
-    except:
-        PowerStatus = 'OFF'
+    command = bootGamingPC
 else:
-    try:
-        # ssh = paramiko.SSHClient()
-        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # ssh.connect(host, SSHPort, username, password)
-        # stdin, stdout, stderr = ssh.exec_command(shutdownGamingPC)
+    command = shutdownGamingPC
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, socketPort))
-        s.sendall(shutdownGamingPC)
-        data = s.recv(1024)
-        s.close()
-
-        PowerStatus = 'OFF'
-    except:
-        PowerStatus = 'ON'
-
-StatusFile = open('/var/www/GamingPCPowerStatus.txt', 'w')
-StatusFile.write(PowerStatus)
-StatusFile.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, socketPort))
+s.sendall(command)
+data = s.recv(1024)
+s.close()
