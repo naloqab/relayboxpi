8b556b7411fc9ce7f9de7297e2872a14b2e8817c
Naser Aloqab <n.aloqab@gmail.com>
2018-12-03 02:33:44 -0600

    Changed the power status script to am infinite 10 second loop

--- GetGamingPCPowerStatus.py
+++ GetGamingPCPowerStatus.py
@@ -1,24 +1,37 @@
 import socket
 import os
+from time import sleep
 
 host = '192.168.1.100'
 socketPort = 5009
 
 command = b"GamingPCPowerStatus"
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, socketPort))
-s.sendall(command)
-data = s.recv(1024)
-s.close()
-
-PowerStatus = data.decode('ascii')
+data = False
 
 logPath = '/var/www/GamingPCPowerStatus.txt'
 
-if not os.path.exists(logPath):
-    os.system("touch {}".format(logPath))
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, socketPort))
+        s.sendall(command)
+        data = s.recv(1024)
+        s.close()
 
-StatusFile = open(logPath, 'w')
-StatusFile.write(PowerStatus)
-StatusFile.close()
+    except Exception as e:
+        print(str(e))
+
+    if data:
+        PowerStatus = data.decode('ascii')
+    else:
+        PowerStatus = "No response"
+
+
+    if not os.path.exists(logPath):
+        os.system("touch {}".format(logPath))
+
+    StatusFile = open(logPath, 'w')
+    StatusFile.write(PowerStatus)
+    StatusFile.close()
+
+    sleep(10)
